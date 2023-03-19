3e5280c391f5aec130c07a42680b29b554873b79
Mahmut <myesil199424@hotmail.com>
2023-03-19 15:54:32 +0300

    state sample Main2

---
+++ src/stateSample/InputStateSample.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function InputStateSample() {
+ 
+    //Inputtaki datayı almak için input üzerine bir state bağlıyorum
+    let metalBands = ['Moonspell', 'Rotting Charist', 'Furtherial'];
+    const [metals, setMetals] = useState(metalBands);
+
+    const [name, setName] = useState('Mahmut');
+    //console.log('rendered');
+  
+ 
+    const newMetalBand=()=>{
+         
+           setMetals([...metals, name]);
+           setName('');
+    }
+
+    const removeAllBands= ()=>{
+        setMetals([]);
+    }
+
+    return (<>
+
+    <ul>
+        {
+            metals.map((item, key)=>{
+               return <li key={key}>{item}</li>
+            })
+        }
+    </ul>
+    <input type='text'value={name} onChange={(e)=>setName(e.target.value)} ></input>
+    <button onClick={()=>newMetalBand()}>Add New Metal Band</button>
+    <button onClick={()=>removeAllBands()}>Remove All Bands new</button>
+    </>)
+}
+
+export default InputStateSample

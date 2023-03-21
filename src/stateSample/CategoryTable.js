15cf1441b7327cc5006f22025308bcb5e780d83b
Mahmut <myesil199424@hotmail.com>
2023-03-21 13:23:37 +0300

    data

--- src/stateSample/CategoryTable.js
+++ src/stateSample/CategoryTable.js
@@ -4,6 +4,7 @@ import { categories } from '../data/categories'
 function CategoryTable() {
     
 const [categoryList, setCategoryList] = useState(categories);
+//const [searchByName, setSearchByName ] = useState('');
 
 const removeAll = ()=>{
     setCategoryList([])
@@ -12,10 +13,55 @@ const removeItem = (id)=>{
     let newCategoryList = categoryList.filter(item=>item.id !== id);
     setCategoryList(newCategoryList);
 }
- 
+const searchCategories = (data)=>{
+  let searchData = data.toLowerCase().trim();
+  let newCategories = categories.filter(q=>q.name.toLowerCase().includes(searchData));
+  setCategoryList(newCategories);
+}
+const orderByAsc = ()=>{
+  let sortCategories = categoryList.sort((a, b) => {
+    let fa = a.name.toLowerCase(),
+        fb = b.name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+});
+setCategoryList([...sortCategories]);
+}
+const orderByDsc = ()=>{
+  let sortCategories = categoryList.sort((a, b) => {
+    let fa = a.name.toLowerCase(),
+        fb = b.name.toLowerCase();
+
+    if (fa < fb) {
+        return 1;
+    }
+    if (fa > fb) {
+        return -1;
+    }
+    return 0;
+});
+setCategoryList([...sortCategories]);
+}
+const loadData = ()=>{
+  setCategoryList(categories);
+}
+
  return (<>
   
-  
+  <div>
+    <input type='text'   onChange={(e)=>searchCategories(e.target.value)} placeholder='Search by name...'></input>   
+  </div>
+  <div>
+    <button onClick={()=>orderByAsc()} >Order By Asc</button>
+    <button onClick={()=>orderByDsc()}>Order By Des</button>
+    <button onClick={()=>loadData()}>Load Data</button>
+  </div>
   <table>
     <tr>
         <td>Id</td>

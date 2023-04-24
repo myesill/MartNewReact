6f2c908832cfb701da70ec9e6d84af738cd8f013
Mahmut <myesil199424@hotmail.com>
2023-04-24 15:48:52 +0300

    routing sample

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
-
-import AddCategoryWithAxious from "./fetchSample/AddCategoryWithAxious";
-
-
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./routerSample/HomePage";
+import AboutPage from "./routerSample/AboutPage";
+import ContactPage from "./routerSample/ContactPage";
+import NoMatch from "./routerSample/NoMatch";
+import ProductList from "./routerSample/ProductList";
+import ProductDetail from "./routerSample/ProductDetail";
 
 
 
@@ -11,9 +13,27 @@ function App() {
 
 
   return (<>
+  <h1>Site Header</h1>
 
-<AddCategoryWithAxious></AddCategoryWithAxious>
+  <ul>
+  <li><Link to='/'>Home</Link></li>
+  <li><Link to='/hakkimizda'>About</Link></li>
+  <li><Link to='/iletisim'>Contact</Link></li>
+  <li><Link to='/urunler'>Products</Link></li>
+ 
 
+</ul>
+
+<Routes>
+ <Route path='/' element={<HomePage/>}></Route>
+ <Route path='/hakkimizda' element= {<AboutPage/>}></Route>
+ <Route path='/iletisim' element={<ContactPage/>}></Route>
+ <Route path='/urunler' element={<ProductList/>}></Route>
+ <Route path='/urunler/:id' element={<ProductDetail/>}></Route>
+ <Route path='*' element={<NoMatch/>}></Route>
+</Routes>
+
+<h1>Site Footer</h1>
 
   </>);
 }
